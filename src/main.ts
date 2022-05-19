c3558bef01a7a2f27c62e329f8e53ca8c828c959
Jurfest <jurfest.apps@gmail.com>
2022-05-18 22:37:05 -0300

    chore: rename file with routes after standalone approach

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
-import { routes } from './app/app-routing.module';
+import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 
