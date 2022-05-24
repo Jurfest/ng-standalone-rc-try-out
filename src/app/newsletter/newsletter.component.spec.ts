0c72634d50b950b30fad4c11435a9ca3f19c10a5
Jurfest <jurfest.apps@gmail.com>
2022-05-24 09:18:14 -0300

    test: fix initial tests

--- src/app/newsletter/newsletter.component.spec.ts
+++ src/app/newsletter/newsletter.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { NewsletterComponent } from './newsletter.component';
 
@@ -8,7 +9,7 @@ describe('NewsletterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ NewsletterComponent ]
+      imports: [ NewsletterComponent, RouterTestingModule ]
     })
     .compileComponents();
 
