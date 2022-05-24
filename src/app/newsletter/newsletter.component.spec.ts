3110b50bba45b61e2acb4230e7343e57e858ca30
Jurfest <jurfest.apps@gmail.com>
2022-05-24 09:45:35 -0300

    test: add tests to newsletter with DOM elements

--- src/app/newsletter/newsletter.component.spec.ts
+++ src/app/newsletter/newsletter.component.spec.ts
@@ -1,4 +1,9 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  ComponentFixtureAutoDetect,
+  TestBed,
+} from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { NewsletterComponent } from './newsletter.component';
@@ -9,9 +14,9 @@ describe('NewsletterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ NewsletterComponent, RouterTestingModule ]
-    })
-    .compileComponents();
+      imports: [NewsletterComponent, RouterTestingModule],
+      providers: [{ provide: ComponentFixtureAutoDetect, useValue: true }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(NewsletterComponent);
     component = fixture.componentInstance;
@@ -20,5 +25,32 @@ describe('NewsletterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show an error message on subscription if email is missing', () => {
+    const button = fixture.debugElement.query(
+      By.css('[data-testid="btn-submit"]')
+    ).nativeElement as HTMLButtonElement;
+    button.click();
+
+    const paragraph = fixture.debugElement.query(By.css('p'))
+      .nativeElement as HTMLParagraphElement;
+    expect(paragraph.textContent).toBe('Missing Email');
+  });
+
+  it('should succeed if email is set', () => {
+    const input = fixture.debugElement.query(By.css('[data-testid=email]'))
+      .nativeElement as HTMLInputElement;
+    input.value = 'email@email.com';
+    input.dispatchEvent(new CustomEvent('input'));
+
+    const button = fixture.debugElement.query(
+      By.css('[data-testid="btn-submit"]')
+    ).nativeElement as HTMLButtonElement;
+    button.click();
+
+    const paragraph = fixture.debugElement.query(By.css('p'))
+      .nativeElement as HTMLParagraphElement;
+    expect(paragraph.textContent).toBe('Thank you for your subscription');
   });
 });
